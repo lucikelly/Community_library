d6a2452fe029dec371f4aed28287056782bd7b18
Lucikelly <lucikelly2001@gmail.com>
2024-10-20 16:59:16 -0300

    finishing project

--- src/service/cron.service.js
+++ src/service/cron.service.js
@@ -3,7 +3,11 @@ import moment from 'moment'
 import sendEmail from './email.service.js'
 import loanRepository from "../repositories/loan.repositories.js"
 
-cron.schedule("42 * * * *" , async () => {
+
+// abaixo minutos = 44, o restante dos * são: horas, dias, meses e anos 
+// (você ainda pode adicionar os segundos opcionalmente antes dos minutos)
+// isso vai agendar o seu envio de email de acordo com sua preferência dos periodos mencionados acima 
+cron.schedule("44 * * * *" , async () => {
   console.log("Running daily job to check for due dates...")
   const loans = await loanRepository.findAllLoansRepository()
   const today = moment().startOf('day')
